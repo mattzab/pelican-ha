808260603d7fa1607cd8613e321405ff37d02f97
Matt Zabojnik <mattzab@gmail.com>
2025-08-02 16:57:58 -0600

    Correct config parsing

--- custom_components/pelican_thermostat/__init__.py
+++ custom_components/pelican_thermostat/__init__.py
@@ -4,22 +4,12 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-import voluptuous as vol
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
-import homeassistant.helpers.config_validation as cv
-from homeassistant import config_entries
 
 from .const import (
-    CONF_BASE_URL,
-    CONF_PASSWORD,
-    CONF_POLL_INTERVAL,
-    CONF_THERMOSTAT_NAME,
-    CONF_USERNAME,
-    DEFAULT_BASE_URL,
     DOMAIN,
 )
 from .coordinator import PelicanThermostatCoordinator
@@ -28,21 +18,8 @@ _LOGGER = logging.getLogger(__name__)
 
 PLATFORMS: list[Platform] = [Platform.CLIMATE, Platform.SENSOR]
 
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_USERNAME): cv.string,
-                vol.Required(CONF_PASSWORD): cv.string,
-                vol.Optional(CONF_BASE_URL, default=DEFAULT_BASE_URL): cv.string,
-                vol.Required(CONF_THERMOSTAT_NAME): cv.string,
-                vol.Optional(CONF_POLL_INTERVAL, default=70): vol.All(
-                    vol.Coerce(int), vol.Range(min=30, max=300)
-                ),
-            }
-        )
-    }
-)
+# Remove the CONFIG_SCHEMA since we're using config flow
+# CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})})
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
